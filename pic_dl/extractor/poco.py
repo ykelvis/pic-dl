daf648f3fd509d65922771d8a0bf5fde81266704
yk <yk@archlinuxcn.org>
2017-01-02 13:19:38 +0800

    fix poco

--- pic_dl/extractor/poco.py
+++ pic_dl/extractor/poco.py
@@ -20,7 +20,3 @@ def return_dic(p):
         pics = _pics[:]
     return {'title': title, 'author': author, 'pics': pics}
 
-if __name__ == '__main__':
-    import requests
-    ret = requests.get('http://my.poco.cn/lastphoto_v2.htx&id=5415252&user_id=57250626&p=0&temp=3853').text
-    print(return_dic(ret))
